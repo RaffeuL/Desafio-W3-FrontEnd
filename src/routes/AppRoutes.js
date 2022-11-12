90d5b095cafaba43a87d6c6ae4d8d10ee568cba5
RaffeuL <rafael91.lisboa@hotmail.com>
2022-11-11 21:39:14 -0300

    use picker

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignUp from "../screens/SignUp";
 import LogIn from "../screens/LogIn";
 import TransferScreen from "../screens/TransferScreen";
+import GiftCardScreen from "../screens/GiftCardScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,12 +13,13 @@ export default function AppRoutes(){
     return(
         <NavigationContainer>
             <Stack.Navigator 
-            initialRouteName= 'TransferScreen'
+            initialRouteName= 'GiftCardScreen'
             screenOptions={{headerShown: false}}>
 
                 <Stack.Screen name='SignUp' component={SignUp}/>
                 <Stack.Screen name='LogIn' component={LogIn}/>
                 <Stack.Screen name='TransferScreen' component={TransferScreen}/>
+                <Stack.Screen name='GiftCardScreen' component={GiftCardScreen}/>
 
             </Stack.Navigator>
         </NavigationContainer>
